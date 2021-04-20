0a906ef3b05451e4ce64b72d4f7df6137f9812d7
Jadhoo <60185715+Jadhoo@users.noreply.github.com>
2021-04-20 10:07:14 +0530

    update controller

--- Controllers/DrugsController.cs
+++ Controllers/DrugsController.cs
@@ -1,4 +1,5 @@
 ﻿using DrugMicroservice.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -10,6 +11,7 @@ namespace DrugMicroservice.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class DrugsController : ControllerBase
     {
         readonly log4net.ILog _log4net;
